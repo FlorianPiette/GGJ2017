a2b1eaaa63df2006429fdb52e2b37dcf8f626053
FloFrancois <f.francois@student.isartdigital.com>
2017-01-21 01:40:41 +0100

    Adding .meta and PlayerMovements

--- GGJ2017/Assets/Scripts/PlayerScript.cs
+++ GGJ2017/Assets/Scripts/PlayerScript.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
 public class PlayerScript : MonoBehaviour
 {
+	[SerializeField]
+    private float speed;
+    //public float tilt;
+	[SerializeField]
+    private Boundary boundary;
+	private Rigidbody2D rigidbody;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rigidbody.velocity = movement * speed;
+
+        rigidbody.position = new Vector2 
+        (
+            Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
+            Mathf.Clamp (rigidbody.position.y, boundary.yMin, boundary.yMax)
+        );
+
+        //rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+    }
 }
