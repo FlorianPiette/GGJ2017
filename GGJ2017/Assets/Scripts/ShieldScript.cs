609f7faf38b364eaed22148f5b722c3adef2883d
RIBOUT Horace <ribouthorace@gmail.com>
2017-01-22 00:26:22 +0100

    phase + shield

--- GGJ2017/Assets/Scripts/ShieldScript.cs
+++ GGJ2017/Assets/Scripts/ShieldScript.cs
@@ -1,21 +1,36 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShieldScript : MonoBehaviour
 {
 	[SerializeField]
-	private int life;
+	private int life=1;
+    public bool is_first_hit = true;
+    public string winnerIntro;
 
 	public void ShieldCollide()
 	{
-		if (life >= 1)
-		{
-			life--;
-		}
-		else
-		{
-			Destroy(gameObject);
-		}
-	}
+        if (is_first_hit)
+        {
+            if (gameObject.transform.position.x<=0)
+            {
+                PhaseManager.Instance.InitiateWinner("p0");
+            }
+            else
+            {
+                PhaseManager.Instance.InitiateWinner("p1");
+            }
+            is_first_hit = false;
+        }else { 
+		    if (life >= 1)
+		    {
+			    life--;
+		    }
+		    else
+		    {
+			    Destroy(gameObject);
+		    }
+        }
+    }
 }
